f2255d77aa228cce2d49789c8ebb70e6d42a61ca
saresend <saresend@usc.edu>
2020-05-25 15:24:44 -0700

    migrating to cpal master branch

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,5 +1,4 @@
 // This contains logic for taking raw bits and encoding it into audio
-use cpal::Format;
 use cpal::SampleFormat;
 
 pub enum EncodedBits {
@@ -8,12 +7,12 @@ pub enum EncodedBits {
     F32(Vec<f32>),
 }
 
-pub fn encode_bits_with_format(data: &[u8], _format: &Format) -> Vec<u8> {
+pub fn encode_bits_with_format(data: &[u8], format: &SampleFormat) -> Vec<u8> {
     data.clone().to_owned()
 }
 
-pub fn package_bits(data: &[u8], format: &Format) -> EncodedBits {
-    match format.data_type {
+pub fn package_bits(data: &[u8], format: &SampleFormat) -> EncodedBits {
+    match format.sample_format {
         SampleFormat::I16 => EncodedBits::I16(data.iter().map(|x| *x as i16).collect()),
         SampleFormat::U16 => EncodedBits::U16(data.iter().map(|x| *x as u16).collect()),
         SampleFormat::F32 => EncodedBits::F32(data.iter().map(|x| (*x as f32).sin()).collect()),
--- src/output.rs
+++ src/output.rs
@@ -1,12 +1,12 @@
-use cpal::Device;
-use async_trait::async_trait;
 use crate::encoding::{encode_bits_with_format, package_bits, EncodedBits};
+use async_trait::async_trait;
 use cpal::traits::DeviceTrait;
+use cpal::Device;
 
 type Meters = f64;
 
 pub struct ReceiverInfo {
-    pub id: String, 
+    pub id: String,
     pub distance: Meters,
 }
 
@@ -18,27 +18,35 @@ pub trait Sender {
 }
 
 pub struct BirdIOutput {
-   device: Device, 
+    device: Device,
 }
 
-
 impl BirdIOutput {
-    async fn play_encoded_bits(&self, data: EncodedBits) -> Result<(), Box<dyn std::error::Error>> {
-
+    fn play_encoded_bits(&self, data: EncodedBits) -> Result<(), Box<dyn std::error::Error>> {
+        let err_fn = |err| println!("Error occurred: {}", err);
+        let config = self.device.default_output_config()?;
+        let outputstream = match data {
+            EncodedBits::I16(val) => {
+                self.device.build_output_stream();
+            
+            }
+            EncodedBits::U16(val) => {}
+            EncodedBits::F32(val) => {}
+        };
+        todo!()
     }
 }
 
 #[async_trait]
 impl Sender for BirdIOutput {
-
     async fn nearby_receivers(&self) -> Result<Vec<ReceiverInfo>, Box<dyn std::error::Error>> {
         todo!()
     }
 
     async fn broadcast_data(&self, info: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-        let fmt = self.device.default_output_format()?;
-        let data = encode_bits_with_format(info, &fmt);
-        let transmit_data = package_bits(&data, &fmt); 
-        self.play_encoded_bits(transmit_data).await
+        let fmt = self.device.default_output_config()?;
+        let data = encode_bits_with_format(info, &fmt.sample_format());
+        let transmit_data = package_bits(&data, &fmt.sample_format());
+        self.play_encoded_bits(transmit_data)
     }
 }
