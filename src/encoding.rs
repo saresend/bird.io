9839652b730ee817f4890e105b38176a71309c2b
saresend <saresend@usc.edu>
2020-05-25 10:44:42 -0700

    beginning trait implementations

---
+++ src/encoding.rs
@@ -0,0 +1,6 @@
+// This contains logic for taking raw bits and encoding it into audio
+use cpal::Format;
+
+pub fn encode_bits_with_format(data: &[u8], _format: Format) -> Vec<u8> {
+    data.clone().to_owned()
+}
---
+++ src/output.rs
@@ -0,0 +1,36 @@
+use cpal::Device;
+use async_trait::async_trait;
+use crate::encoding::encode_bits_with_format;
+use cpal::traits::DeviceTrait;
+
+type Meters = f64;
+
+pub struct ReceiverInfo {
+    pub id: String, 
+    pub distance: Meters,
+}
+
+// This will contain logic for outbound data transmission
+#[async_trait]
+pub trait Sender {
+    async fn nearby_receivers(&self) -> Result<Vec<ReceiverInfo>, Box<dyn std::error::Error>>;
+    async fn broadcast_data(&self, info: &[u8]) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+pub struct BirdIOutput {
+   device: Device, 
+}
+
+#[async_trait]
+impl Sender for BirdIOutput {
+
+    async fn nearby_receivers(&self) -> Result<Vec<ReceiverInfo>, Box<dyn std::error::Error>> {
+        todo!()
+    }
+
+    async fn broadcast_data(&self, info: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+        let fmt = self.device.default_output_format()?;
+        let data = encode_bits_with_format(info, fmt);
+        todo!()
+    }
+}
