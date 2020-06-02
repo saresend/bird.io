b45f2598d0136a8c3bd57ac2928b8ee6fc5f1ded
saresend <saresend@usc.edu>
2020-06-01 18:55:30 -0700

    adding file extension

--- src/bin/report.rs
+++ src/bin/report.rs
@@ -3,5 +3,5 @@ fn main() {
     let driver = output::BirdIOutput::default();
     driver.play_low_freq(); // This should store data
     let data = instrumentation::get_data(0);
-    let _ = instrumentation::visualize_pcm(&data, "low frequency visualized");
+    let _ = instrumentation::visualize_pcm(&data, "low_freq.png");
 }
