b911fd48c5cd023836995a9da0eb5f5cf8443acf
saresend <saresend@usc.edu>
2020-06-04 12:08:11 -0700

    adding to reporting function

--- src/bin/report.rs
+++ src/bin/report.rs
@@ -4,4 +4,8 @@ fn main() {
     driver.play_low_freq(); // This should store data
     let data = instrumentation::get_data(0);
     let _ = instrumentation::visualize_pcm(&data, "low_freq.png");
+
+    driver.test_dasp_sine();
+    let data = instrumentation::get_data(1);
+    let _ = instrumentation::visualize_pcm(&data, "dasp_sine.png");
 }
