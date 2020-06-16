1259418eb0dba16ff345cdd3c47026260579c546
saresend <saresend@usc.edu>
2020-06-15 18:23:00 -0700

    beginning to develop windowed fft strategy

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,6 @@
 // This will contain logic for reading in input
 use crate::strategy::Strategy;
-use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::Device;
 
 pub struct BirdIInput {
@@ -16,13 +16,46 @@ impl BirdIInput {
         BirdIInput { device }
     }
 
+    pub fn create_handler_fn<T: cpal::Sample>() -> impl Fn(&[T], &cpal::InputCallbackInfo) {
+        return move |data: &[T], _: &cpal::InputCallbackInfo| {
+            println!("{}", data.len());
+        };
+    }
+
     pub fn recv<K: Strategy>(&self, strategy: K) {
-        let _config: cpal::SupportedStreamConfig =
+        let config: cpal::SupportedStreamConfig =
             self.device.default_input_config().unwrap().into();
-        //let err_fn = |err| println!("{}", err);
-        todo!()
+
+        let err_fn = |err| println!("{}", err);
+        let input_stream = match config.sample_format() {
+            cpal::SampleFormat::F32 => self.device.build_input_stream(
+                &config.config(),
+                BirdIInput::create_handler_fn::<f32>(),
+                err_fn,
+            ),
+            cpal::SampleFormat::I16 => self.device.build_input_stream(
+                &config.config(),
+                BirdIInput::create_handler_fn::<i16>(),
+                err_fn,
+            ),
+            cpal::SampleFormat::U16 => self.device.build_input_stream(
+                &config.config(),
+                BirdIInput::create_handler_fn::<u16>(),
+                err_fn,
+            ),
+        }
+        .unwrap();
+        input_stream.play().unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::*;
+    #[test]
+    fn sanity_input_test() {
+        let receiver = input::BirdIInput::default();
+        receiver.recv(strategy::NaiveFrequencyModulation {});
+    }
+}
--- src/strategy.rs
+++ src/strategy.rs
@@ -1,6 +1,7 @@
 // Contains the trait for strategy Logic
 //
 use dasp::{signal, Signal};
+use dft::{Operation, Plan};
 
 pub trait Strategy {
     fn encode_bits<T: cpal::Sample>(&self, data: &[u8]) -> Vec<T>;
@@ -9,9 +10,17 @@ pub trait Strategy {
 
 pub struct NaiveFrequencyModulation {}
 
+fn decode_chunk(mut value: Vec<f32>) -> u8 {
+    let plan = Plan::new(Operation::Forward, value.len());
+    dft::transform(&mut value, &plan);
+    // Now in theory we have a frequency distribution for the given window
+
+    return 0;
+}
+
 impl Strategy for NaiveFrequencyModulation {
     fn encode_bits<T: cpal::Sample>(&self, data: &[u8]) -> Vec<T> {
-        let threshold = 1_000; // Take 20,000 samples per bit
+        let threshold = 1_000;
         let mut low_signal = signal::rate(44100.0).const_hz(5000.0).sine();
         let mut high_signal = signal::rate(44100.0).const_hz(10000.0).sine();
         let mut result_vec: Vec<T> = vec![];
@@ -28,7 +37,12 @@ impl Strategy for NaiveFrequencyModulation {
         }
         return result_vec;
     }
+
     fn decode_bits<T: cpal::Sample>(&self, data: &[T]) -> Vec<u8> {
-        todo!()
+        let encoded_data: Vec<f32> = data.iter().map(|x| x.to_f32()).collect();
+        let threshold = 1_000;
+        let owned_data: Vec<Vec<f32>> =
+            encoded_data.chunks(threshold).map(|x| x.to_vec()).collect();
+        owned_data.into_iter().map(|x| decode_chunk(x)).collect()
     }
 }
