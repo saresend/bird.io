63ef0671480f3c0ed1941b50e90d8198d8c30084
saresend <saresend@usc.edu>
2020-07-13 17:31:20 -0700

    untested implementation of yin algorithm, here we go

--- src/freq_estimation.rs
+++ src/freq_estimation.rs
@@ -1,14 +1,9 @@
-/*
-* use ndarray::prelude::*;
-* use ndarray::Array;
-*/
-
-fn diff_function(audio_sample: &[u8], tau_max: usize) -> Vec<u64> {
-    let mut diff_vec = vec![0; tau_max];
+fn diff_function(audio_sample: &[u8], tau_max: usize) -> Vec<f64> {
+    let mut diff_vec = vec![0.0; tau_max];
     for tau in 1..tau_max {
         for j in 0..(audio_sample.len() - tau_max) {
             let tmp = audio_sample[j] - audio_sample[j + tau];
-            diff_vec[tau] += tmp as u64 * tmp as u64;
+            diff_vec[tau] += tmp as f64 * tmp as f64;
         }
     }
     diff_vec
@@ -18,7 +13,10 @@ fn cum_mean_norm_diff_fn(df: Vec<f64>) -> Vec<f64> {
     let mut iter1 = df.iter().clone();
     iter1.next();
     let total: f64 = iter1.sum();
-    vec![1.0].into_iter().chain(df.into_iter()).collect()
+    vec![1.0]
+        .into_iter()
+        .chain(df.into_iter().map(|y| y / total))
+        .collect()
 }
 
 fn get_pitch(cmdf: Vec<f64>, tau_min: usize, tau_max: usize) -> usize {
@@ -35,9 +33,16 @@ fn get_pitch(cmdf: Vec<f64>, tau_min: usize, tau_max: usize) -> usize {
     return 0;
 }
 
-fn compute_yin(signal: Vec<u8>, sr: usize, freq_min: usize, freq_max: usize) -> Vec<f64> {
-    let tau_min = sr / freq_max;
+pub fn compute_yin_for_frame(
+    signal: Vec<u8>,
+    sr: usize,
+    freq_min: usize,
+    freq_max: usize,
+) -> usize {
     let tau_max = sr / freq_min;
+    let tau_min = sr / freq_max;
+    let df = diff_function(&signal, tau_max);
+    let cmdf = cum_mean_norm_diff_fn(df);
 
-    todo!()
+    get_pitch(cmdf, tau_min, tau_max)
 }
