08a12092ae82406f43e3ab133568a6ba09ac77a3
saresend <saresend@usc.edu>
2020-07-21 11:32:56 -0700

    ok now cmndf seems to be working

--- src/freq_estimation.rs
+++ src/freq_estimation.rs
@@ -10,17 +10,22 @@ fn diff_function(audio_sample: &[f64], tau_max: usize) -> Vec<f64> {
 }
 
 fn cmndf(raw_diff: &[f64]) -> Vec<f64> {
-    let mut denom: Vec<f64> = raw_diff[1..]
+    use crate::instrumentation::*;
+    let mut cmndf_diff: Vec<f64> = raw_diff[1..]
         .iter()
         .enumerate()
         .scan(0.0, |state, x| {
             *state = *state + x.1;
-            let result = x.1 * x.0 as f64 / *state;
+            let result = *x.1 * (x.0 + 1) as f64 / *state;
             Some(result)
         })
         .collect();
-    denom.insert(0, 1.0);
-    denom
+    let thing: Vec<f32> = cmndf_diff.clone().iter().map(|x| *x as f32).collect();
+    cmndf_diff.insert(0, 1.0);
+    println!("{:?}", &raw_diff[1..]);
+    println!("{:?}", cmndf_diff);
+    save_data(&thing, 2);
+    cmndf_diff
 }
 
 fn compute_diff_min(diff_fn: &[f64], max_tau: usize, harm_threshold: f64) -> usize {
@@ -45,15 +50,29 @@ fn convert_to_frequency(sample_period: usize, sample_rate: usize) -> f64 {
 // should return a tau that gives the # of elements of offset in a given sample
 fn compute_sample_frequency(audio_sample: Vec<f64>) -> f64 {
     let tau_max = audio_sample.len() * 25 / 1000;
+    let tau_max = 50;
     let diff_fn = diff_function(&audio_sample, tau_max);
-    let sample_period = compute_diff_min(&diff_fn, tau_max, 0.1);
+    let cmndf = cmndf(&diff_fn);
+    let sample_period = compute_diff_min(&cmndf, tau_max, 0.1);
     convert_to_frequency(sample_period, audio_sample.len())
 }
 
 #[cfg(test)]
 mod tests {
+    use super::super::instrumentation::*;
     use super::{compute_diff_min, compute_sample_frequency, convert_to_frequency, diff_function};
     use dasp::{signal, Signal};
+
+    #[test]
+    fn visualize_cmndf() {
+        let sample_rate = 120;
+        let mut sine_sample = signal::rate(sample_rate as f64).const_hz(3.0).sine();
+        let sample_frame: Vec<f64> = (0..sample_rate).map(|_| sine_sample.next()).collect();
+        let result = compute_sample_frequency(sample_frame);
+        let cmndf_data = get_data(2);
+        visualize_pcm(&cmndf_data, "cmndf_output.png");
+        assert_eq!(result, 3.0);
+    }
 
     #[test]
     fn diff_function_basic_sine() {
@@ -62,6 +81,32 @@ mod tests {
         let sample_frame: Vec<f64> = (0..sample_rate).map(|_| sine_sample.next()).collect();
         let result = diff_function(&sample_frame, 6);
         assert_eq!(result, vec![0.0, 6.0, 12.0, 6.0, 0.0, 6.0]);
+    }
+
+    #[test]
+    fn conv_noisey_sr_12_f_3() {
+        use rand::Rng;
+        let sample_rate = 120;
+        let mut sine_sample = signal::rate(sample_rate as f64).const_hz(3.0).sine();
+        let mut rng = rand::thread_rng();
+        let sample_frame: Vec<f64> = (0..sample_rate)
+            .map(|_| sine_sample.next() + rng.gen::<f64>())
+            .collect();
+        let result = compute_sample_frequency(sample_frame);
+        assert_eq!(result, 3.0);
+    }
+
+    #[test]
+    fn conv_noisey_sine() {
+        use rand::Rng;
+        let sample_rate = 44100;
+        let mut sine_sample = signal::rate(sample_rate as f64).const_hz(441.0).sine();
+        let mut rng = rand::thread_rng();
+        let sample_frame: Vec<f64> = (0..sample_rate)
+            .map(|_| sine_sample.next() + rng.gen::<f64>() * 0.005)
+            .collect();
+        let result = compute_sample_frequency(sample_frame);
+        assert_eq!(result, 441.0);
     }
 
     #[test]
