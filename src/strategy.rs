b874ec332d26c7f95e858cf1133c03bae6895572
saresend <saresend@usc.edu>
2020-07-21 15:32:02 -0700

    todo for tmo: actually incorporate yin and do more tests

--- src/strategy.rs
+++ src/strategy.rs
@@ -2,43 +2,34 @@
 //
 use crate::instrumentation;
 use dasp::{signal, Signal};
+use yin::Yin;
 
 pub trait Strategy {
     fn encode_bits<T: cpal::Sample>(&self, data: &[u8]) -> Vec<T>;
     fn decode_bit<T: cpal::Sample>(&mut self, data: &[T]) -> u8;
 }
 
-pub struct NaiveFrequencyModulation {}
+pub struct NaiveFrequencyModulation {
+    estimator: Yin,
+}
 
 impl NaiveFrequencyModulation {
     pub fn default() -> Self {
-        Self {}
+        Self {
+            estimator: Yin::init(0.1, 3000.0, 10000.0, 44100),
+        }
     }
 
-    fn pad_vec(mut original: Vec<f32>) -> Vec<f32> {
-        let length = original.len();
-        let mut containing_size = 1;
-        while containing_size < length {
-            containing_size *= 2;
-        }
-        let remainder = containing_size - length;
-        for _ in 0..remainder {
-            original.push(0.0);
-        }
-        original
-    }
-
-    fn convert_to_bit(sample: Vec<f32>) -> u8 {
-        instrumentation::save_data(&sample, 0);
-        0
+    pub fn convert_to_bit(frequency: f64) -> u8 {
+        return 0;
     }
 }
 
 impl Strategy for NaiveFrequencyModulation {
     fn encode_bits<T: cpal::Sample>(&self, data: &[u8]) -> Vec<T> {
         let threshold = 1_000;
-        let mut low_signal = signal::rate(44100.0).const_hz(5000.0).sine();
-        let mut high_signal = signal::rate(44100.0).const_hz(10000.0).sine();
+        let mut low_signal = signal::rate(44100.0).const_hz(4410.0).sine();
+        let mut high_signal = signal::rate(44100.0).const_hz(8820.0).sine();
         let mut result_vec: Vec<T> = vec![];
         for val in data {
             if val != &0 {
@@ -55,8 +46,10 @@ impl Strategy for NaiveFrequencyModulation {
     }
 
     fn decode_bit<T: cpal::Sample>(&mut self, data: &[T]) -> u8 {
-        let encoded_data: Vec<f32> = data.iter().map(|x| x.to_f32()).collect();
+        let encoded_data: Vec<f64> = data.iter().map(|x| x.to_f32() as f64).collect();
+        let frequency = self.estimator.estimate_freq(&encoded_data);
         // we then need to pad encoded_data to the nearest power of 2
-        NaiveFrequencyModulation::convert_to_bit(encoded_data)
+        // we need some error handling here
+        NaiveFrequencyModulation::convert_to_bit(frequency)
     }
 }
