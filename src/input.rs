5f530074c01ee244aeca62763e04500ee9e2ba3c
saresend <saresend@usc.edu>
2020-07-23 12:03:14 -0700

    input methods almost finished

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,7 @@
 // This will contain logic for reading in input
 use crate::errors::DeviceNotFoundError;
 use crate::frequency_modulation::NaiveFrequencyModulation;
-use crate::traits::BirdReceiver;
+use crate::traits::{BirdReceiver, Strategy};
 use cpal::traits::{DeviceTrait, HostTrait};
 use std::sync::mpsc::{channel, Receiver, Sender};
 
@@ -12,19 +12,60 @@ impl BirdListener {
         Self {}
     }
 
-    fn create_stream_with_config(
+    fn create_input_handler<'a, T, K>(
         &self,
-        config: cpal::SupportedStreamConfig,
-    ) -> Result<cpal::Stream, Box<dyn std::error::Error>> {
-        todo!()
+        strategy: K,
+    ) -> impl FnMut(&[T], &cpal::InputCallbackInfo) + 'a
+    where
+        T: cpal::Sample,
+        K: Strategy + 'a,
+    {
+        |values, thing| {}
     }
 
-    fn setup_receiver(&self) -> Result<cpal::Stream, Box<dyn std::error::Error>> {
+    fn create_error_handler(&self) -> impl Fn(cpal::StreamError) {
+        |err| println!("{:?}", err)
+    }
+
+    fn create_stream_with_config<'a, T>(
+        &self,
+        device: cpal::Device,
+        config: cpal::SupportedStreamConfig,
+        strategy: T,
+    ) -> Result<cpal::Stream, Box<dyn std::error::Error>>
+    where
+        T: Strategy + 'a,
+    {
+        let error_fn = self.create_error_handler();
+        let stream = match config.sample_format() {
+            cpal::SampleFormat::F32 => device.build_input_stream(
+                &config.config(),
+                self.create_input_handler::<f32, T>(strategy),
+                error_fn,
+            )?,
+            cpal::SampleFormat::I16 => device.build_input_stream(
+                &config.config(),
+                self.create_input_handler::<i16, T>(strategy),
+                error_fn,
+            )?,
+            cpal::SampleFormat::U16 => device.build_input_stream(
+                &config.config(),
+                self.create_input_handler::<u16, T>(strategy),
+                error_fn,
+            )?,
+        };
+        Ok(stream)
+    }
+
+    fn setup_receiver<'a, T>(&self, strategy: T) -> Result<cpal::Stream, Box<dyn std::error::Error>>
+    where
+        T: Strategy + 'a,
+    {
         let device = cpal::default_host()
             .default_input_device()
             .ok_or(DeviceNotFoundError)?;
         let config = device.default_input_config()?;
-        let stream = self.create_stream_with_config(config)?;
+        let stream = self.create_stream_with_config(device, config, strategy)?;
         Ok(stream)
     }
 
@@ -35,12 +76,12 @@ impl BirdListener {
 use std::thread;
 impl BirdReceiver<NaiveFrequencyModulation> for BirdListener {
     type BitFormat = u8;
-    fn start(
+    fn start<'a>(
         &self,
         strategy: NaiveFrequencyModulation,
     ) -> Result<Receiver<Self::BitFormat>, Box<dyn std::error::Error>> {
         let (sender, receiver) = channel::<Self::BitFormat>();
-        let values = self.setup_receiver();
+        let play_stream = self.setup_receiver(strategy);
         let handler = thread::spawn(move || {
             Self::open_receive(sender);
         });
