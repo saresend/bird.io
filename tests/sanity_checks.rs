40f124d1e432b5b61e3553bbcd5966588b86a5b4
saresend <saresend@usc.edu>
2020-07-29 18:05:04 -0700

    beginning integration testing

---
+++ tests/sanity_checks.rs
@@ -0,0 +1,22 @@
+use birdio::frequency_modulation::*;
+use birdio::traits::*;
+use birdio::{input, output};
+
+#[test]
+fn test_basics() {
+    use std::thread;
+    use std::time::Duration;
+
+    let mut input = input::BirdListener::default();
+    let strategy = NaiveFrequencyModulation::default();
+    let input_channel = input.start(strategy).unwrap();
+    thread::spawn(|| {
+        thread::sleep(Duration::from_millis(100));
+        let example_bits = vec![0, 1, 0, 1, 0, 1];
+        let mut strategy = NaiveFrequencyModulation::default();
+        let output = output::BirdIOutput::default();
+        output.transmit(strategy, &example_bits).unwrap();
+    });
+    let output = input_channel.recv().unwrap();
+    println!("{:?}", output);
+}
